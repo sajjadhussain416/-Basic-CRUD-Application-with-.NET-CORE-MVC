80d0e1df122bbf690bf984e869c05761369ee764
Sajjad Hussain <104603874+sajjadhussain416@users.noreply.github.com>
2023-02-27 22:59:33 +0500

    done crud

---
+++ Src/CRUDApplication-MVC/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDApplication_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDApplication_MVC.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
